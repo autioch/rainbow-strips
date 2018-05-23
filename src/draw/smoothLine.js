fc8216d3db1f37301baef680104eb950c97905a3
Autioch <autioch@gmail.com>
2018-05-23 22:39:04 +0200

    Smooth line refactor.

--- src/draw/smoothLine.js
+++ src/draw/smoothLine.js
@@ -1,26 +1,6 @@
-/* eslint-disable max-lines */
-/* eslint-disable max-len */
 /* eslint-disable max-depth */
 /* eslint-disable max-statements */
-/* eslint-disable complexity */
-/* eslint-disable no-undefined */
-/* eslint-disable no-bitwise */
-/* eslint-disable no-shadow */
-/* eslint-disable no-var */
-/* eslint-disable func-style */
-/* eslint-disable func-names */
-/* eslint-disable one-var */
-/* eslint-disable sort-vars */
 /* eslint-disable no-mixed-operators */
-/* eslint-disable block-scoped-var */
-/* eslint-disable no-redeclare */
-/* eslint-disable vars-on-top */
-/* eslint-disable no-inner-declarations */
-/* eslint-disable no-undef */
-/* eslint-disable prefer-destructuring */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-negated-condition */
-/* eslint-disable no-use-before-define */
 
 // This is my own smoothing method
 // It creates a set of bezier control points either 2nd order or third order
@@ -31,36 +11,37 @@
 // match: if true then the control points will be balanced.
 // Function will make a copy of the points
 
-export default function smoothLine(points, cornerThres, match) { // adds bezier control points at points if lines have angle less than thres
-  var p1,
-      p2,
-      p3,
-      dist1,
-      dist2,
-      x,
-      y,
-      endP,
-      len,
-      angle,
-      i,
-      newPoints,
-      aLen,
-      closed,
-      bal,
-      cont1,
-      nx1,
-      nx2,
-      ny1,
-      ny2,
-      np;
+// adds bezier control points at points if lines have angle less than thres
+export default function smoothLine(points, cornerThres, match) {
+  let p1;
+  let p2;
+  let p3;
+  let dist1;
+  let dist2;
+  let x;
+  let y;
+  let endP;
+  let len;
+  let angle;
+  let i;
+  let isClosed;
+  let nx1;
+  let nx2;
+  let ny1;
+  let ny2;
 
-  function dot(x, y, xx, yy) { // get do product
+  function dot(point, current, next) { // get do product
+    const cx = current[0] - point[0];
+    const cy = current[1] - point[1];
+    const xx = next[0] - current[0];
+    const yy = next[1] - current[1];
+
     // dist1,dist2,nx1,nx2,ny1,ny2 are the length and  normals and used outside function
     // normalise both vectors
-    dist1 = Math.sqrt(x * x + y * y); // get length
+    dist1 = Math.sqrt(cx * cx + cy * cy); // get length
     if (dist1 > 0) { // normalise
-      nx1 = x / dist1;
-      ny1 = y / dist1;
+      nx1 = cx / dist1;
+      ny1 = cy / dist1;
     } else {
       nx1 = 1; // need to have something so this will do as good as anything
       ny1 = 0;
@@ -76,8 +57,10 @@ export default function smoothLine(points, cornerThres, match) { // adds bezier 
 
     return Math.acos(nx1 * nx2 + ny1 * ny2); // dot product
   }
-  newPoints = []; // array for new points
-  aLen = points.length;
+
+  const newPoints = []; // array for new points
+  const aLen = points.length;
+
   if (aLen <= 2) { // nothing to if line too short
     for (i = 0; i < aLen; i++) { // ensure that the points are copied
       newPoints.push([points[i][0], points[i][1]]);
@@ -85,22 +68,26 @@ export default function smoothLine(points, cornerThres, match) { // adds bezier 
 
     return newPoints;
   }
-  p1 = points[0];
+
+  [p1] = points;
   endP = points[aLen - 1];
-  i = 0; // start from second poitn if line not closed
-  closed = false;
+  i = 0; // start from second poitn if line not isClosed
+  isClosed = false;
   len = Math.hypot(p1[0] - endP[0], p1[1] - endP[1]);
+
   if (len < Math.SQRT2) { // end points are the same. Join them in coordinate space
     endP = p1;
-    i = 0; // start from first point if line closed
+    i = 0; // start from first point if line isClosed
     p1 = points[aLen - 2];
-    closed = true;
+    isClosed = true;
   }
+
   newPoints.push([points[i][0], points[i][1]]);
+
   for (; i < aLen - 1; i++) {
     p2 = points[i];
     p3 = points[i + 1];
-    angle = Math.abs(dot(p2[0] - p1[0], p2[1] - p1[1], p3[0] - p2[0], p3[1] - p2[1]));
+    angle = Math.abs(dot(p1, p2, p3));
     if (dist1 !== 0) { // dist1 and dist2 come from dot function
       if (angle < cornerThres * 3.14) { // bend it if angle between lines is small
         if (match) {
@@ -118,7 +105,7 @@ export default function smoothLine(points, cornerThres, match) { // adds bezier 
           x /= len;
           y /= len;
           if (newPoints.length > 0) {
-            var np = newPoints[newPoints.length - 1];
+            const np = newPoints[newPoints.length - 1];
 
             np.push(p2[0] - x * dist1 * 0.25);
             np.push(p2[1] - y * dist1 * 0.25);
@@ -136,7 +123,8 @@ export default function smoothLine(points, cornerThres, match) { // adds bezier 
     }
     p1 = p2;
   }
-  if (closed) { // if closed then copy first point to last.
+
+  if (isClosed) { // if isClosed then copy first point to last.
     p1 = [];
     for (i = 0; i < newPoints[0].length; i++) {
       p1.push(newPoints[0][i]);
